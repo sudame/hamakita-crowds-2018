2f44db2e61ea6888fedb826917f886fc6f399b22
SUDA Mikihiro <su.glasses.923@gmail.com>
2018-05-14 13:23:11 +0900

    feat: initialize firebase

--- src/main.js
+++ src/main.js
@@ -2,12 +2,18 @@
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue';
 import SuiVue from 'semantic-ui-vue';
+import firebase from 'firebase';
+import VueFire from 'vuefire';
 import App from './App';
 import router from './router';
+import firebaseKey from '../firebase.key'
 
 Vue.config.productionTip = false;
 
 Vue.use(SuiVue);
+Vue.use(Vuefire);
+
+const firebaseApp = firebase.initializeApp(firebaseKey);
 
 /* eslint-disable no-new */
 new Vue({
