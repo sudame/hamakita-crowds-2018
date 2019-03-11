06b41b7a5d771f80a1d31b433ea921813e26a156
sudame <su.glasses.923@gmail.com>
2019-03-11 23:31:32 +0900

    add: remove securty keys

--- firebase.key.js
+++
@@ -1,8 +0,0 @@
-export default {
-  apiKey: "AIzaSyCDK3a3OEt5DfLa1G63_jdrjWuAEFbKDE0",
-  authDomain: "hamakita-crowds-2018.firebaseapp.com",
-  databaseURL: "https://hamakita-crowds-2018.firebaseio.com",
-  projectId: "hamakita-crowds-2018",
-  storageBucket: "hamakita-crowds-2018.appspot.com",
-  messagingSenderId: "1023736634196"
-}
